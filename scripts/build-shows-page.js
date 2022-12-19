3361923e23cb55dd06dc8c624ca1423b3ec53e26
Logan Henrie <loghenrie@gmail.com>
2022-12-18 23:56:23 -0800

    add row selection and hover styles to shows

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -40,6 +40,7 @@ function renderShows() {
 		let row = document.createElement("div");
 		row.classList.add("shows__row");
 
+		//just add each property because we're using nth child css
 		Object.getOwnPropertyNames(show).forEach((prop) => {
 			let div = document.createElement("div");
 			let p = document.createElement("p");
@@ -48,12 +49,27 @@ function renderShows() {
 			row.appendChild(div);
 		});
 
+		//create the buy tickets button
 		let button = document.createElement("button");
 		button.classList.add("shows__button");
 		button.innerText = "Buy Tickets";
 		row.appendChild(button);
 
+		//add the event listener for row selection
+		row.addEventListener("click", select);
+
 		showsDiv.appendChild(row);
-		showsDiv.appendChild(document.createElement("hr"));
 	});
+}
+
+function select(e) {
+	if (!e.currentTarget.classList.contains("shows__row--selected")) {
+		let hits = document.querySelectorAll(".shows__row--selected");
+		if (hits.length > 0) {
+			hits.forEach((hit) => {
+				hit.classList.remove("shows__row--selected");
+			});
+		}
+		e.currentTarget.classList.add("shows__row--selected");
+	}
 }
